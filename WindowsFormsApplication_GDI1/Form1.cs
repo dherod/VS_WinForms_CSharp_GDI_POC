81ad866e7b6e0ce30aa8fa4d2ca1bf7a3d891a87
Don Herod <don.herod@gmail.com>
2013-11-03 00:28:54 -0400

    Draw some stuff using GDI and GDI+

---
+++ WindowsFormsApplication_GDI1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication_GDI1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // GDI+
+            DrawUsingGdiPlus(e);
+
+            // GDI
+            DrawUsingGdi(e);
+        }
+
+        private unsafe void DrawUsingGdi(PaintEventArgs e)
+        {
+            // Get handle to device context.
+            IntPtr hdc = e.Graphics.GetHdc();
+
+            // Draw rectangle with GDI using default pen.
+            GDI.Rectangle(hdc, 10, 70, 110, 120);
+
+            // Create new pen to match web site
+            int newPenColor = 0x00913d0b; // 0x00BBGGRR from frsoft.com
+            int newPenStyle = 0; // PS_SOLID from wingdi.h
+            int newPenWidth = 3;
+            IntPtr hNewPen = GDI.CreatePen(newPenStyle, newPenWidth, newPenColor);
+            IntPtr hPrevPen = GDI.SelectObject(hdc, hNewPen);
+
+            // Draw lines to create logo
+            GDI.POINT point = new GDI.POINT();
+            GDI.MoveToEx(hdc, 100, 0, &point);
+            GDI.LineTo(hdc, 0, 100);
+            GDI.LineTo(hdc, 100, 200);
+
+            // Clean up
+            GDI.SelectObject(hdc, hPrevPen);
+            GDI.DeleteObject(hNewPen);
+            e.Graphics.ReleaseHdc(hdc);
+        }
+
+        private void DrawUsingGdiPlus(PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Red, 7);
+            e.Graphics.DrawLine(pen, 1, 1, 100, 100);
+            e.Graphics.DrawLine(pen, 100, 1, 200, 100);
+        }
+    }
+}
---
+++ WindowsFormsApplication_GDI1/GDI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication_GDI1
+{
+    public class GDI
+    {
+        public struct POINT
+        {
+            long x;
+            long y;
+        }
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        internal static extern bool Rectangle(
+           IntPtr hdc,
+           int ulCornerX, int ulCornerY,
+           int lrCornerX, int lrCornerY);
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        internal static extern IntPtr CreatePen(
+            int fnPenStyle,
+            int nWidth,
+            int crColor //0x00BBGGRR
+        );
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        internal static extern IntPtr SelectObject(
+            IntPtr hdc,
+            IntPtr hgdiobj
+        );
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        internal static extern bool DeleteObject(
+            IntPtr hObject
+        );
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        internal static extern unsafe bool MoveToEx(
+          IntPtr hdc,
+          int X,
+          int Y,
+          POINT* lppoint
+        );
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        internal static extern bool LineTo(
+           IntPtr hdc,
+           int nXEnd,
+           int nYEnd
+        );
+    }
+}
